164eeaba0785552cba3a4fc5f521b07f0f6bac61
michael kyalo <michaelkyalo876@gmail.com>
2025-06-11 14:27:43 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = "Grace Hopper";
+
+// Create the welcomeMessage using the currentUser
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Create the excitedWelcomeMessage in uppercase and with an exclamation mark
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Create the shortGreeting using only the first initial of currentUser
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
